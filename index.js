aaf83668632fc3dbf508ef353836eaae8083154a
himalayanschool1995 <himalayanschool1995@gmail.com>
2021-08-15 18:10:36 +0545

    project init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "success",
+  });
+});
+
+
+app.listen(PORT)
